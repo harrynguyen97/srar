c0cc90ae8ce028496f61a31fc398d7281e1e96b4
harrynguyen97 <harry.nguyen1597@gmail.com>
2018-08-08 19:53:17 +0930

    Change variable name and help() for fitting with compression/decompression purpose

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char* argv[])
 	if (argc > 1 && argc <= 5) {
 		std::string option = argv[1];
 
-		if (option == "-e") {
+		if (option == "-c") {
 			try{
 				std::string source_file_path = argv[2];
 				std::string dest_file_path = argv[3];
@@ -25,22 +25,19 @@ int main(int argc, char* argv[])
 				ofstream headerFile;
 				ofstream dataFile;
 				
-				std::cout << "Encrypting [" << source_file_path << "]...\n";
+				std::cout << "Compressing [" << source_file_path << "]...\n";
 				compress(inFile, headerFile, dataFile, 
-						 source_file_path, dest_file_path, h_extension::tom);
+						 source_file_path, dest_file_path, h_extension::har);
 				std::cout << "Finished.\n";
 				std::cout << "Saved at: " << dest_file_path << std::endl;
 				inFile.close();
 				headerFile.close();
 				dataFile.close();
-			}
-			catch (const std::logic_error) {
+			} catch (const std::logic_error) {
 				std::cout << "[Error!] You may not enter the path correctly.\n";
 				std::cout << "Type \"srar --help\" for more info.\n"; 
 			}
-		}
-
-		else if (option == "-j") {
+		} else if (option == "-j") {
 			try {
 				std::string header_file_path = argv[2];
 				std::string data_file_path = argv[3];
@@ -62,14 +59,11 @@ int main(int argc, char* argv[])
 				headerFile.close();
 				dataFile.close();
 				joinedFile.close();
-			}
-			catch (const std::logic_error) {
+			} catch (const std::logic_error) {
 				std::cout << "[Error!] You may not enter the path correctly.\n";
 				std::cout << "Type \"srar --help\" for more info.\n"; 
 			}
-		}
-
-		else if (option == "-d") {
+		} else if (option == "-d") {
 			try {
 				std::string source_file_path = argv[2];
 				std::string dest_file_path = argv[3];
@@ -79,20 +73,17 @@ int main(int argc, char* argv[])
 
 				ofstream decompressedFile;
 
-				std::cout << "Decrypting [" << source_file_path << "]...\n";
+				std::cout << "Decompressing [" << source_file_path << "]...\n";
 				decompress(inFile, decompressedFile, source_file_path, dest_file_path);
 				std::cout << "Finished.\n";
 				std::cout << "Saved at: " << dest_file_path << std::endl;
 				inFile.close();
 				decompressedFile.close();
-			}
-			catch (const std::logic_error) {
+			} catch (const std::logic_error) {
 				std::cout << "[Error!] You may not enter the path correctly.\n";
 				std::cout << "Type \"srar --help\" for more info.\n"; 
 			}
-		}
-
-		else if (option == "--help") {
+		} else if (option == "--help") {
 			help();
 		}
 
@@ -115,8 +106,8 @@ void help()
 	std::cout << "Version: SRAR v1.0\n";
 	std::cout << "Hello from 2018.\n\n";
 	std::cout << "USAGE: \n";
-	std::cout << "For encryption: srar -e <path_to_file_to_be_compressed> <path_to_dest_dir>\n";
-	std::cout << "For decryption: srar -d <path_to_compressed_file> <path_to_dest_dir>\n";
+	std::cout << "For compression: srar -c <path_to_file_to_be_compressed> <path_to_dest_dir>\n";
+	std::cout << "For decompression: srar -d <path_to_compressed_file> <path_to_dest_dir>\n";
 	std::cout << "For joining 2 files: srar -j <path_to_first_file> <path_to_second_file> <path_to_dest_dir>\n";
 	std::cout << "For showing this messsage: srar --help\n";
 }
